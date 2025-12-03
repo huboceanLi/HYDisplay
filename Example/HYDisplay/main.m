70afaa28f7de98ba7a34ca82fbaf039c259f6ef1
OceanLi <li437277219@gmail.com>
2025-12-03 21:04:41 +0800

    Initial commit

---
+++ Example/HYDisplay/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HYDisplay
+//
+//  Created by li437277219@gmail.com on 12/03/2025.
+//  Copyright (c) 2025 li437277219@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+#import "HYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HYAppDelegate class]));
+    }
+}
